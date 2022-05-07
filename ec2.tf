db6bc2ff163d50063b0b0044ca035fb77f086a8f
pashkadez <pashkadez@gmail.com>
2022-05-08 00:58:55 +0300

    terraform instance config

--- ec2.tf
+++ ec2.tf
@@ -25,9 +25,11 @@ resource "aws_instance" "tailscale" {
   key_name = aws_key_pair.ssh_key.key_name
   security_groups = [aws_security_group.allow_all.id]
   subnet_id = aws_subnet.my_subnet.id
+  associate_public_ip_address = true
   tags = {
     Name = "tailscale"
   }
+
   /* depends_on = [
     aws_subnet.my_subnet
   ] */
