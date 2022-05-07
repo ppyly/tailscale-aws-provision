ce248b8cd7fc149cdb0b558784f5f8d5b808d62f
pashkadez <pashkadez@gmail.com>
2022-05-07 20:51:59 +0300

    tools installation script created

---
+++ tools-install.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+set -e
+
+# install ansible
+sudo apt update
+sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+# install aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# install terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform -y
