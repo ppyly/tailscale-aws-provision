e02b573314067f861fd3521d6395c1fd64d36c6f
pashkadez <pashkadez@gmail.com>
2022-05-08 17:14:10 +0300

    working on ssh key creation and usage fix

--- configure.sh
+++ configure.sh
@@ -1,16 +1,19 @@
 #/bin/bash
-set -e
+# set -e
 
 DIR=${PWD}
 echo "Generating ssh key"
 ssh-keygen -f id_rsa_tf -t rsa -N ''
 ssh_pub_key=$(cat id_rsa_tf.pub)
+
+
+# 
+echo "AWS-cli configuration, input KEY ID and ACCESS KEY, to skip other parameters hit ENTER"
+# aws configure
+
+echo "Creating envvars.sh"
+cat << EOF > envvars.sh
 export TF_VAR_public_key="${ssh_pub_key}"
 export TF_VAR_ssh_key_path="${PWD}/id_rsa_tf"
-
- 
-echo "AWS-cli configuration, input KEY ID and ACCESS KEY, to skip other parameters hit ENTER"
-aws configure
-
-
+EOF
 
