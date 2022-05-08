44b4dcbe2399b64dd18a139ac33ee05c17ed8d9f
pashkadez <pashkadez@gmail.com>
2022-05-08 17:35:18 +0300

    terraform instance config

--- configure.sh
+++ configure.sh
@@ -1,19 +1,17 @@
 #/bin/bash
-# set -e
+set -e
 
 DIR=${PWD}
 echo "Generating ssh key"
 ssh-keygen -f id_rsa_tf -t rsa -N ''
 ssh_pub_key=$(cat id_rsa_tf.pub)
 
-
-# 
 echo "AWS-cli configuration, input KEY ID and ACCESS KEY, to skip other parameters hit ENTER"
-# aws configure
+aws configure
 
 echo "Creating envvars.sh"
 cat << EOF > envvars.sh
 export TF_VAR_public_key="${ssh_pub_key}"
-export TF_VAR_ssh_key_path="${PWD}/id_rsa_tf"
+export TF_VAR_ssh_key_path="${DIR}/id_rsa_tf"
 EOF
 
