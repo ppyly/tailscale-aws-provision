35d46b315db5809cd018502175720ae116352ae4
pashkadez <pashkadez@gmail.com>
2022-05-08 18:13:28 +0300

    terraform instance config

--- ec2.tf
+++ ec2.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "tailscale" {
     connection {
       host        = aws_instance.tailscale.public_ip
       type        = "ssh"
-      user        = "var.ssh_user"
+      user        = var.ssh_user
       private_key = file(var.ssh_key_path)
     }
   }
